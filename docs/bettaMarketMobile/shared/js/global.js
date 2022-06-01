5bd50db2aed665b85eba03b11cdd68f460e2f118
mario2111 <nvtweb@gmail.com>
2022-06-01 22:15:21 +0700

    gggg

--- docs/bettaMarketMobile/shared/js/global.js
+++ docs/bettaMarketMobile/shared/js/global.js
@@ -6,6 +6,24 @@ $(document).ready(function(){
     history.back(1);
   });
  
+  ////// modal top sheet //////
+  var curentSheet;
+  $(".callModal").each(function() {
+    $(this).click(function(event) {
+      event.preventDefault();
+      curentSheet = $($(this).attr("href"));
+      showModalSheet(curentSheet);
+    });
+  });
+  
+  function showModalSheet(curentSheet) {
+    curentSheet.parent().addClass('active');
+    $('body').css('overflow','hidden');
+  }
+  $('.overlay-modal').click(function() {
+    $(this).parent().removeClass('active');
+    $('body').removeAttr("style");
+  });
   
 });
 
